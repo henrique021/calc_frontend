32c6f03bd94a1f91926117ac3e51af3bb3ea93dd
henrique021 <51052419+henrique021@users.noreply.github.com>
2025-04-20 15:29:57 -0300

    Update single-panel.component.ts

--- src/app/single-panel/single-panel.component.ts
+++ src/app/single-panel/single-panel.component.ts
@@ -3,10 +3,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-panel',
-  imports: [ReactiveFormsModule,HttpClientModule],
+  imports: [ReactiveFormsModule,HttpClientModule,CommonModule],
   templateUrl: './single-panel.component.html',
   styleUrl: './single-panel.component.css'
 })
